738cdc89e4fb6ef58816f0a2acf54fcc9f584661
vishvabamniya-rgb <vishvabamniya@gmail.com>
2026-02-25 00:46:30 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Deploy Your App to Heroku
+
+[![Deploy to heroku chacha](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?template=https://github.com/kabaci1299/hotty.git)
+
---
+++ vars.py
@@ -0,0 +1,80 @@
+import os
+from os import environ
+
+# API Configuration
+API_ID = int(os.environ.get("API_ID", "37138438"))
+API_HASH = os.environ.get("API_HASH", "d1b5f3a54b0c8130747432aaca542b77")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+CREDIT = os.environ.get("CREDIT","𝐒нɑᎥ𝚝ɑη❤️‍🔥")
+
+# MongoDB Configuration
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "CpprivateApi")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://itsgoluAPI:jrMHSipToKUEnmcp@cpprivateapi.ghhp3oz.mongodb.net/?apnpName=CpprivateApi")  # Add your own atlas db
+MONGO_URL = DATABASE_URL  # For auth system
+
+# Owner and Admin Configuration
+OWNER_ID = int(os.environ.get("OWNER_ID", "8306791018"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "8306791018").split()]  # Default to owner ID
+
+# Channel Configuration
+PREMIUM_CHANNEL = ""
+# Thumbnail Configuration
+THUMBNAILS = list(map(str, os.environ.get("THUMBNAILS", "").split())) # Image Link For Default Thumbnail 
+
+# Web Server Configuration
+WEB_SERVER = os.environ.get("WEB_SERVER", "False").lower() == "true"
+WEBHOOK = True  # Don't change this
+PORT = int(os.environ.get("PORT", 8000))
+
+# Message Formats
+AUTH_MESSAGES = {
+    "subscription_active": """<b>🎉 Subscription Activated!</b>
+
+<blockquote>Your subscription has been activated and will expire on {expiry_date}.
+You can now use the bot!</blockquote>\n\n Type /start to start uploading """,
+
+    "subscription_expired": """<b>⚠️ Your Subscription Has Ended</b>
+
+<blockquote>Your access to the bot has been revoked as your subscription period has expired.
+Please contact the admin to renew your subscription.</blockquote>""",
+
+    "user_added": """<b>✅ User Added Successfully!</b>
+
+<blockquote>👤 Name: {name}
+🆔 User ID: {user_id}
+📅 Expiry: {expiry_date}</blockquote>""",
+
+    "user_removed": """<b>✅ User Removed Successfully!</b>
+
+<blockquote>User ID {user_id} has been removed from authorized users.</blockquote>""",
+
+    "access_denied": """<b>⚠️ Access Denied!</b>
+
+<blockquote>You are not authorized to use this bot.
+Please contact the admin to get access.</blockquote>""",
+
+    "not_admin": "⚠️ You are not authorized to use this command!",
+    
+    "invalid_format": """❌ <b>Invalid Format!</b>
+
+<blockquote>Use format: {format}</blockquote>"""
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
